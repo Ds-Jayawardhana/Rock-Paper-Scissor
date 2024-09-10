fad6748e1eaa1c5ac2c78d0cb7dd23af7dfae5c5
Disandu Sanhida Jayawardhane <dissaaa12345@gmail.com>
2024-09-10 22:26:47 +0530

    sd

--- Scripts/11-todo-list.js
+++ Scripts/11-todo-list.js
@@ -1,4 +1,4 @@
-const todoList=[''];
+const todoList=[{}];
 
 
 
@@ -7,8 +7,20 @@ function RenderTodoList(){
 
 
 for(let i=0;i<todoList.length;i++){
-    const todoLists=todoList[i];
-    const html=`<p>${todoLists}</p>`;
+    const todoObject=todoList[i];
+    //const name=todoObject.name;
+    //const dueDate=todoObject.dueDate;
+    const { name,dueDate }=todoObject;//Shortcut for the line 17 & 18
+    const html=`
+        <div class="dtls">${name}</div>
+        <div class="dtls">${dueDate}</div>
+        <button onclick="
+            todoList.splice(${i},1);
+            RenderTodoList();
+        "class="dlt-btn">Delete</button>
+
+    
+    `;
     todoListHTML+=html;
 }
 console.log(todoListHTML);
@@ -23,9 +35,17 @@ document.querySelector('.js-todo-list')
 function addTodo(){
     const inputelement=document.querySelector('.js-name-input');
     const name=inputelement.value;
+
+    const dateInputElement=document.querySelector('.js-duedate-input');
+    const dueDate=dateInputElement.value;
     
 
-    todoList.push(name);
+    todoList.push({
+        //name:name,
+        //dueDate:dueDate,
+        name,
+        dueDate
+    });
     console.log(todoList);
 
     inputelement.value='';
